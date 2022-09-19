49a07d708708391a70e96aec8d3046d13438a304
Marcelo Avanzi <marcelodaniel@hotmail.com>
2022-09-19 20:50:44 -0300

    Adicionando Editar e Deletar na lista de Pets e tambem barra de busca por nome

--- usuario/urls.py
+++ usuario/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ListaPetsUsuarios,CadastrarPet
+from .views import EditarPet, ListaPetsUsuarios,CadastrarPet,EditarPet,DeletarPet
 
 
 urlpatterns = [
     path('',ListaPetsUsuarios.as_view(),name='usuario.home'),
-    path('cadastrarpet/',CadastrarPet.as_view(),name='usuario.cadastrarpet')
+    path('cadastrarpet/',CadastrarPet.as_view(),name='usuario.cadastrarpet'),
+    path('editarpet/<int:pk>',EditarPet.as_view(),name='usuario.editarpet'),
+    path('deletarpet/<int:pk>',DeletarPet.as_view(),name='usuario.deletarpet'),
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView,CreateView
+from django.views.generic import ListView,CreateView,UpdateView,DeleteView
 from .models import PetsUsuario
 from .forms import PetForm
 
@@ -7,7 +7,26 @@ class ListaPetsUsuarios(ListView):
     model = PetsUsuario
     queryset = PetsUsuario.objects.all().order_by('nome_completo') #dentro da tabela Pessoa, pega tudo oq tem na lista 
 
+    #Aplicar filtros na busca
+    def get_queryset(self):
+        queryset =  super().get_queryset()
+        #queryset = queryset.filter(usuario=self.request.user)
+        filtro_nome = self.request.GET.get('filtronome') or None
+        if filtro_nome:
+            queryset = queryset.filter(nome_completo__contains=filtro_nome)
+
+        return queryset
+
 class CadastrarPet(CreateView):
     model = PetsUsuario
     form_class = PetForm
+    success_url = '/usuario/'
+
+class EditarPet(UpdateView):
+    model = PetsUsuario
+    form_class = PetForm
+    success_url = '/usuario/'
+
+class DeletarPet(DeleteView):
+    model = PetsUsuario
     success_url = '/usuario/'
