c8f50b5659c8ea27cf325c9b076b0d0586e87475
Marcelo Avanzi <marcelodaniel@hotmail.com>
2023-02-28 21:10:04 -0300

    Pagina de mensalidade linkada com checkout da empresa pagarme

--- planos/urls.py
+++ planos/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from .views import test
+from .views import Home,pagarme_page
 
 
 urlpatterns=[
-    path('',login_required(test),name='planos.home'),
+    path('',login_required(Home),name='planos.home'),
+    path('pagarme/',login_required(pagarme_page),name='planos.pagarmepage'),
 ]
--- planos/views.py
+++ planos/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def test(request):
+def Home(request):
     return render(request,'planos.html')
+
+def pagarme_page(request):
+    return render(request,'pagarme.html')
