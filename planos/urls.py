2672d2db15520a0ab3d7a5f42217d3c6f2b549cb
Marcelo Avanzi <marcelodaniel@hotmail.com>
2023-03-01 21:03:13 -0300

    Inserido pra cada plano o seu devido checkout

--- planos/urls.py
+++ planos/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from .views import Home,pagarme_page
+from .views import Home
 
 
 urlpatterns=[
     path('',login_required(Home),name='planos.home'),
-    path('pagarme/',login_required(pagarme_page),name='planos.pagarmepage'),
 ]
--- planos/views.py
+++ planos/views.py
@@ -3,7 +3,5 @@ from django.http import HttpResponse
 
 
 def Home(request):
+    
     return render(request,'planos.html')
-
-def pagarme_page(request):
-    return render(request,'pagarme.html')
